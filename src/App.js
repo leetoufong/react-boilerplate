b9852fbec9d7584d539476ed62f41fc6d902fb6e
Lee <pla9@cdc.gov>
2024-12-09 15:28:14 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () =>{
+    return (
+        <>
+            <h1>Hello world. Welcome to a React App Boilerplate.</h1>
+            <p>This boilerplate does not utilize Creact React App (CRA).</p>
+        </>
+    )
+}
+
+export default App;
